9fbcde48048abc5d6e3f9944eff8996f68becb7d
donaldma <donald.ma@hotmail.ca>
2017-07-05 04:58:22 +0000

    add exercise 9

--- lib/store.rb
+++ lib/store.rb
@@ -2,4 +2,11 @@ class Store < ActiveRecord::Base
   has_many :employees
   validates :name, length: { minimum: 3}
   validates_numericality_of :annual_revenue, :greater_than_or_equal_to => 0
+
+  before_destroy :has_employee?
+
+  def has_employee?
+    errors.add(:destroy, "is not allowed to store with employees") if employees.count > 0
+    errors.blank?
+  end
 end
